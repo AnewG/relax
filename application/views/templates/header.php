c5af889af0060de54199229140df12decf7ee6fb
AnewG <breakitwall@gmail.com>
2018-03-09 18:11:35 +0800

    add favicon

---
+++ application/views/templates/header.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+    	<link href="data:image/x-icon;base64,AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAQAABILAAASCwAAAAAA
+AAAAAADz9Pj/9PXq//Tz7f/z8/D/8/Tz//T19P/09fb/9Pb0//b5+f/2+fn/9vr5//b5+P/0+vj/
++e7l//Xv7v/y8/P/NFvn/z9mjP9Ia1D/SWlJ/0ppQf9AXTf/UW16/1KIZ/9kuI//YbON/2Gzjv9m
+tZP/XraY/4uUlP86Kin/EBQV/xZL7f8WS+//HVPS/xxPy/8lVYb/Nlkv/zNKdv8xToT/Voik/3a+
+wv9rubH/W7GU/0Sthv92jrP/Mh0T/wAAAf8dTOj/HEvs/yJQ4v8oQbH/UXnc/6PR9/+XwvT/eZnQ
+/1dzwP9xh7//ncLs/5vI6/9EhY7/Ml7b/3xrjf+KYlH/G0fk/xhE5/8jS87/RDtV/1Butf+TvvD/
+p9f//6vd//94jav/R053/3d1df+myOP/RFq6/xBC6v8oOYn/Rywt/xpD4P8XQOH/Pj1q/0g7TP9T
+c7j/lL7s/6XU//+RuOT/cIGg/1M/Pv96Z1X/lbLJ/1RSbv8XQdr/Njp0/0A0U/8gRM3/KkKu/1g0
+Hf9WXYP/gKzl/6nY//+j1P//l7vb/1tXZv9gQSr/f1st/5izzf9+msD/KUGk/zA9iP9MLif/Fzra
+/0k8W/9ZMRT/cpC//67h//+hz/r/seL//5zJ+v9kTUH/dUoW/5BkI/97gpD/QmGt/1lljP8nMIX/
+VTEd/xA25v9FPm3/XTQP/1lkmf+fy/T/lsf0/5+rzP9eZXX/U0Aw/5RqLf+qgDT/dnuI/zdbtf9s
+dIn/NiZO/08yLf8SNdv/ODh6/1YvEP82L0b/irLY/1l2sv85Q3T/JjRa/2lPMP+bdDf/uJFE/3hl
+UP9Ya5v/YE5F/z8pPv9IM0H/IDi2/0I2VP9KJxf/UlNq/4mlxf95kLT/VV+J/0lIbf+LZTH/oXs7
+/72aUv9wRxr/WDAQ/14zCf9GLzz/QDVb/yoyk/9ZNhz/USoT/2dXVP9zgJr/iabI/3FmYP9ULhP/
+lW80/7OPSP+5lk//cUsh/144Gf97URr/SjE5/0MwTP8jLpr/ZT4Y/2U8Fv9/Z0v/fnVu/393b/+Z
+dTr/dUwc/6WCRP+/nFP/updQ/7GNRv+RazT/iWAm/2dFKv9GKSj/ECe7/1c/P/+HVxf/n3o9/6yK
+Sv+0lFP/t5NL/5duMP+5llD/vJlS/6F7O/+4lU7/uZNK/4ZdJP9cNhj/Sysk/wsetf8cIo//g1ok
+/7WOQf+5l1D/updP/7ORS/+0kUr/updQ/7OPSP+ddTP/m3U4/5lvLf9/WCL/TC4i/08tH/8bJKb/
+EB60/zc2hv+ddj3/sIpH/6mHSf+zkk//t5dT/7aVUv+ykE3/on9C/5VvNv94UST/b15P/39jQP+S
+b0D/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAA==" rel="icon" type="image/x-icon" />
+        <title>Title</title>
+    </head>
+    <body>
+
+        <h1>from header.php</h1>
